0c35e9d145a909709a6b4cd831e5c549dfdacdd1
rockjake <42801875+rockjake@users.noreply.github.com>
2020-11-16 19:18:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/updateing/minieap package/minieap #校园网认证登录_新
+git clone https://github.com/githuba/xmurp-ua package/xmurp-ua #修改UA_过校园网多终端
+git clone https://github.com/hyrathb/mentohust package/mentohust #修改版mentohust支持V4认证
